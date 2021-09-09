8d38607706031fc8b4c84ea03e011975e71971ee
Anevear <anevear2180@gmail.com>
2021-09-08 22:13:32 -0500

    handsOn update

--- AngularL04/src/app/app.module.ts
+++ AngularL04/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SkillsService } from './services/skills.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SkillsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
